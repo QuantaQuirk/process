833ef951d542712bb4d344a4a0b04cc9aa0d6017
Tomáš Selič (Smisch-DEV) <contact@smisch.sk>
2023-10-13 18:27:37 +0200

    v1.0.0

---
+++ Exceptions/ProcessFailedException.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace QuantaQuirk\Process\Exceptions;
+
+use QuantaQuirk\Contracts\Process\ProcessResult;
+use RuntimeException;
+
+class ProcessFailedException extends RuntimeException
+{
+    /**
+     * The process result instance.
+     *
+     * @var \QuantaQuirk\Contracts\Process\ProcessResult
+     */
+    public $result;
+
+    /**
+     * Create a new exception instance.
+     *
+     * @param  \QuantaQuirk\Contracts\Process\ProcessResult  $result
+     * @return void
+     */
+    public function __construct(ProcessResult $result)
+    {
+        $this->result = $result;
+
+        $error = sprintf('The command "%s" failed.'."\n\nExit Code: %s",
+            $result->command(),
+            $result->exitCode(),
+        );
+
+        if (! empty($result->output())) {
+            $error .= sprintf("\n\nOutput:\n================\n%s", $result->output());
+        }
+
+        if (! empty($result->errorOutput())) {
+            $error .= sprintf("\n\nError Output:\n================\n%s", $result->errorOutput());
+        }
+
+        parent::__construct($error, $result->exitCode() ?? 1);
+    }
+}
